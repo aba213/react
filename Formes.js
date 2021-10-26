9fc114c08e73da806c60b79ed82afb7bb1f8df97
aba213 <amelameur2018@gmail.com>
2021-10-26 15:46:07 +0100

    first commit

---
+++ Formes.js
@@ -0,0 +1,40 @@
+import react from 'react'
+import {Form,Row, Col, Button, Container} from 'react-bootstrap'
+
+function Formes() {
+    return (
+            <div className='Formes'>
+              <Container>
+                
+           <Form>
+  <Form.Group as={Row} controlId="formHorizontalEmail">
+    <Form.Label column sm={2}>
+      Email
+    </Form.Label>
+    <Col sm={10}>
+      <Form.Control type="email" placeholder="Email" />
+    </Col>
+  </Form.Group>
+
+  <Form.Group as={Row} controlId="formHorizontalPassword">
+    <Form.Label column sm={2}>
+      Password
+    </Form.Label>
+    <Col sm={10}>
+      <Form.Control type="password" placeholder="Password" />
+    </Col>
+  </Form.Group>
+  
+  <Form.Group as={Row} controlId="formHorizontalCheck">
+    <Col sm={{ span: 10, offset: 2 }}>
+      <Form.Check label="Remember me" />
+    </Col>
+  </Form.Group>
+</Form>
+</Container>
+            </div>
+    );
+    
+}
+
+export default Formes;
